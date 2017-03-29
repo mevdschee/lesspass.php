4034aeafc590fdaa8474c9f59df1dcc4f0ea0f4b
Maurits van der Schee <maurits@vdschee.nl>
2017-03-29 20:19:35 +0200

    whitespace

--- tests/ApiTests.php
+++ tests/ApiTests.php
@@ -5,8 +5,7 @@ require_once __DIR__.'/../lesspass.php';
 
 class ApiTests extends TestCase
 {
-    public function testRenderPassword()
-    {
+    public function testRenderPassword() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -15,8 +14,7 @@ class ApiTests extends TestCase
         $this->assertEquals('WHLpUL)e00[iHR+w', $generatedPassword);
     }
 
-    public function testRenderPasswordNoSymbols()
-    {
+    public function testRenderPasswordNoSymbols() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -25,25 +23,16 @@ class ApiTests extends TestCase
         $this->assertEquals('MBAsB7b1Prt8Sl', $generatedPassword);
     }
 
-    public function testRenderPasswordOnlyDigits()
-    {
+    public function testRenderPasswordOnlyDigits() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
-        $passwordProfile = [
-            'length'=>6,
-            'counter'=>3,
-            'lowercase'=>false,
-            'uppercase'=>false,
-            'numbers'=>true,
-            'symbols'=>false
-        ];
+        $passwordProfile = ['length'=>6, 'counter'=>3, 'lowercase'=>false, 'uppercase'=>false, 'symbols'=>false];
         $generatedPassword = generatePassword($site, $login, $masterPassword, $passwordProfile);
         $this->assertEquals('117843', $generatedPassword);
     }
 
-    public function testRenderPasswordNoNumbers()
-    {
+    public function testRenderPasswordNoNumbers() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -52,8 +41,7 @@ class ApiTests extends TestCase
         $this->assertEquals('sB>{qF}wN%/-fm', $generatedPassword);
     }
 
-    public function testRenderPasswordWithDefaultOptions()
-    {
+    public function testRenderPasswordWithDefaultOptions() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
--- tests/EntropyTests.php
+++ tests/EntropyTests.php
@@ -5,8 +5,7 @@ require_once __DIR__.'/../lesspass.php';
 
 class EntropyTests extends TestCase
 {
-    public function testCalcEntropyPbkdf2WithDefaultParams()
-    {
+    public function testCalcEntropyPbkdf2WithDefaultParams() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -15,23 +14,16 @@ class EntropyTests extends TestCase
         $this->assertEquals('dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e', $entropy);
     }
 
-    public function testCalcEntropyWithDifferentOptions()
-    {
+    public function testCalcEntropyWithDifferentOptions() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
-        $passwordProfile = getPasswordProfile([
-            'iterations'=>8192,
-            'keylen'=>16,
-            'digest'=>'sha512',
-            'counter'=>1
-        ]);
+        $passwordProfile = getPasswordProfile(['iterations'=>8192, 'keylen'=>16, 'digest'=>'sha512']);
         $entropy = calcEntropy($site, $login, $masterPassword, $passwordProfile);
         $this->assertEquals('fff211c16a4e776b3574c6a5c91fd252', $entropy);
     }
 
-    public function testCalcEntropyWithCounterOne()
-    {
+    public function testCalcEntropyWithCounterOne() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -41,8 +33,7 @@ class EntropyTests extends TestCase
     }
 
 
-    public function testCalcEntropyWithCounterTwo()
-    {
+    public function testCalcEntropyWithCounterTwo() {
         $site = 'example.org';
         $login = 'contact@example.org';
         $masterPassword = 'password';
@@ -51,8 +42,7 @@ class EntropyTests extends TestCase
         $this->assertEquals('ddfb1136260f930c21f6d72f6eddbd40', $entropy);
     }
 
-    public function testConsumeEntropy()
-    {
+    public function testConsumeEntropy() {
         list($value,$entropy) = consumeEntropy('', gmp_init(4 * 4 + 2), "abcd", 2);
         $this->assertEquals('ca', $value);
         $this->assertEquals(1, (int)$entropy);
