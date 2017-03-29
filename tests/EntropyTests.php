01c74d1b9198702ecb9c251fd744b6284b62d9fd
Maurits van der Schee <maurits@vdschee.nl>
2017-03-29 18:54:34 +0200

    unit tests

--- tests/EntropyTests.php
+++ tests/EntropyTests.php
@@ -1,58 +1,77 @@
-var assert = chai.assert;
+<?php
+use PHPUnit\Framework\TestCase;
 
-describe('LessPass v2', function() {
-  describe('entropy', function() {
-    it('calc entropy pbkdf2 with default params (100000 iterations, 32 bytes length, sha256 digest)', function() {
-      this.timeout(10000);
-      var site = 'example.org';
-      var login = 'contact@example.org';
-      var masterPassword = 'password';
-      var passwordProfile = {
-        iterations: 100000,
-        keylen: 32,
-        digest: 'sha256',
-        counter: 1
-      };
-      return LessPass._calcEntropy(site, login, masterPassword, passwordProfile).then(function(entropy) {
-        assert.equal('dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e', entropy);
-      });
-    });
-    it('calc entropy with different options (8192 iterations, 16 bytes length, sha512 digest)', function() {
-      var site = 'example.org';
-      var login = 'contact@example.org';
-      var masterPassword = 'password';
-      var passwordProfile = {
-        iterations: 8192,
-        keylen: 16,
-        digest: 'sha512',
-        counter: 1
-      };
-      return LessPass._calcEntropy(site, login, masterPassword, passwordProfile).then(function(entropy) {
-        assert.equal('fff211c16a4e776b3574c6a5c91fd252', entropy);
-      });
-    });
-    it('calc entropy different if counter different 1', function() {
-      var site = 'example.org';
-      var login = 'contact@example.org';
-      var masterPassword = 'password';
-      var passwordProfile1 = {iterations: 1, keylen: 16, digest: 'sha256', counter: 1};
-      return LessPass._calcEntropy(site, login, masterPassword, passwordProfile1).then(function(entropy) {
-        assert.equal('d3ec1e988dd0b3640c7491cd2c2a88b5', entropy)
-      });
-    });
-    it('calc entropy different if counter different 2', function() {
-      var site = 'example.org';
-      var login = 'contact@example.org';
-      var masterPassword = 'password';
-      var passwordProfile2 = {iterations: 1, keylen: 16, digest: 'sha256', counter: 2};
-      return LessPass._calcEntropy(site, login, masterPassword, passwordProfile2).then(function(entropy) {
-        assert.equal('ddfb1136260f930c21f6d72f6eddbd40', entropy)
-      });
-    });
-    it('consume entropy', function() {
-      var password = LessPass._consumeEntropy('', bigInt(4 * 4 + 2), "abcd", 2);
-      assert.equal('ca', password.value);
-      assert.equal(1, password.entropy)
-    });
-  });
-});
+require_once __DIR__.'/../lesspass.php';
+
+class ApiTests extends TestCase
+{
+    private function getPasswordProfile($passwordProfile)
+    {
+        $defaultPasswordProfile = (object)[
+            'lowercase'=>true,
+            'uppercase'=>true,
+            'numbers'=>true,
+            'symbols'=>true,
+            'digest'=>'sha256',
+            'iterations'=>100000,
+            'keylen'=>32,
+            'length'=>16,
+            'counter'=>1,
+            'version'=>2
+        ];
+        return (object)array_merge((array)$defaultPasswordProfile,(array)$passwordProfile);
+    }
+
+    public function testCalcEntropyPbkdf2WithDefaultParams()
+    {
+        $site = 'example.org';
+        $login = 'contact@example.org';
+        $masterPassword = 'password';
+        $passwordProfile = $this->getPasswordProfile([]);
+        $entropy = calcEntropy($site, $login, $masterPassword, $passwordProfile);
+        $this->assertEquals('dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e', $entropy);
+    }
+
+    public function testCalcEntropyWithDifferentOptions()
+    {
+        $site = 'example.org';
+        $login = 'contact@example.org';
+        $masterPassword = 'password';
+        $passwordProfile = $this->getPasswordProfile([
+            'iterations'=>8192,
+            'keylen'=>16,
+            'digest'=>'sha512',
+            'counter'=>1
+        ]);
+        $entropy = calcEntropy($site, $login, $masterPassword, $passwordProfile);
+        $this->assertEquals('fff211c16a4e776b3574c6a5c91fd252', $entropy);
+    }
+
+    public function testCalcEntropyWithCounterOne()
+    {
+        $site = 'example.org';
+        $login = 'contact@example.org';
+        $masterPassword = 'password';
+        $passwordProfile = $this->getPasswordProfile(['iterations'=>1,'keylen'=>16]);
+        $entropy = calcEntropy($site, $login, $masterPassword, $passwordProfile);
+        $this->assertEquals('d3ec1e988dd0b3640c7491cd2c2a88b5', $entropy);
+    }
+
+
+    public function testCalcEntropyWithCounterTwo()
+    {
+        $site = 'example.org';
+        $login = 'contact@example.org';
+        $masterPassword = 'password';
+        $passwordProfile = $this->getPasswordProfile(['iterations'=>1,'keylen'=>16,'counter'=>2]);
+        $entropy = calcEntropy($site, $login, $masterPassword, $passwordProfile);
+        $this->assertEquals('ddfb1136260f930c21f6d72f6eddbd40', $entropy);
+    }
+
+    public function testConsumeEntropy()
+    {
+        list($value,$entropy) = consumeEntropy('', gmp_init(4 * 4 + 2), "abcd", 2);
+        $this->assertEquals('ca', $value);
+        $this->assertEquals(1, (int)$entropy);
+    }
+}
